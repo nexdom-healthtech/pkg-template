7961f50d61e58db88bd5ab0fe8722335c1d3b5d9
rafael-perini <perini.de.souza.rafael@gmail.com>
2026-06-29 20:31:14 +0000

    test: update message validations

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -2,5 +2,5 @@ import { expect, test } from "vite-plus/test";
 import { sayHello } from "@/index.ts";
 
 test("sayHello", () => {
-  expect(sayHello()).toBe("Hello, NEXTER!");
+  expect(sayHello()).toBe("Hello, NEXTERS!");
 });
