06d4655509a313df89fa1b341c0ecadbc9dba380
rafael-perini <perini.de.souza.rafael@gmail.com>
2026-07-23 13:04:30 +0000

    test(depcruise): prefer peer deps

--- .dependency-cruiser.cjs
+++ .dependency-cruiser.cjs
@@ -76,16 +76,15 @@ module.exports = {
       },
     },
     {
-      name: "peer-deps-used",
-      comment:
-        "This module depends on an npm package that is declared as a peer dependency " +
-        "in your package.json. This makes sense if your package is e.g. a plugin, but in " +
-        "other cases - maybe not so much. If the use of a peer dependency is intentional " +
-        "add an exception to your dependency-cruiser configuration.",
+      name: "use-peer-deps",
+      comment: `Since this project is a library, you should prefer to use peer dependencies.`,
       severity: "error",
-      from: {},
+      from: {
+        path: ["src"],
+        pathNot: ["__tests__"],
+      },
       to: {
-        dependencyTypes: ["npm-peer"],
+        dependencyTypesNot: ["local", "npm-peer"],
       },
     },
   ],
